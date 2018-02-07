979aafd242796990300c86c52a9dbabbb126542e
anmeng.wei <anmeng.wei@31huiyi.com>
2018-02-07 18:01:00 +0800

    am

---
+++ AmBlitz/Extensions/QueryableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AmBlitz.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> query, int pageIndex, int pageSize)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            return query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        }
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, int, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+    }
+}
