780132f7e08857000d5954d065b39eb2fb7f9909
anmeng.wei <anmeng.wei@31huiyi.com>
2018-02-08 08:58:01 +0800

    am

--- AmBlitz/Domain/EntityDescribe.cs
+++ AmBlitz/Domain/EntityDescribe.cs
@@ -18,6 +18,7 @@ namespace AmBlitz.Domain
         /// 启用主从 并且属性值为true读从库
         /// </summary>
         public bool ReadSecondary { get; set; }
+
         /// <summary>
         /// 是否支持软删除对象
         /// </summary>
