70c2d5805d3d1205ad7e46ee378c3c373ead697c
anmeng.wei <anmeng.wei@31huiyi.com>
2018-02-08 09:40:13 +0800

    am

---
+++ nupkg/pack-amblitz.bat
@@ -0,0 +1,3 @@
+"NuGet.exe" "pack" "..\AmBlitz\AmBlitz.csproj" -Properties Configuration=Debug -IncludeReferencedProjects
+"NuGet.exe" "pack" "..\AmBlitz.Mongo\AmBlitz.Mongo.csproj" -Properties Configuration=Debug -IncludeReferencedProjects
+"NuGet.exe" "pack" "..\AmBlitz.RedisCache\AmBlitz.RedisCache.csproj" -Properties Configuration=Debug -IncludeReferencedProjects
