d4e51fd7330245e69cf7077ffee8075691c415c6
anmeng.wei <anmeng.wei@31huiyi.com>
2018-02-26 13:43:46 +0800

    am

--- AmBlitz.RedisCache/RedisClient.cs
+++ AmBlitz.RedisCache/RedisClient.cs
@@ -9,17 +9,18 @@ namespace AmBlitz.RedisCache
     public class RedisClient:ICache, ISingletonDependency
     {
         private ConnectionMultiplexer _conn;
-        private readonly string connect;
+        private readonly string _connect;
+        private const string Prefix = "amblitz";
         public RedisClient(AmBlitzConfiguration blitzConfiguration)
         {
-            connect = blitzConfiguration.RedisConnectionString();
+            _connect = blitzConfiguration.RedisConnectionString();
         }
 
         private IDatabase GetDatabase()
         {
             if (_conn == null)
             {
-                _conn = ConnectionMultiplexer.Connect(connect);
+                _conn = ConnectionMultiplexer.Connect(_connect);
             }
             return _conn.GetDatabase();
         }
@@ -27,24 +28,20 @@ namespace AmBlitz.RedisCache
         public long Decrement(string key, long value)
         {
             var db = GetDatabase();
-            return db.StringDecrement(key, value);
+            return db.StringDecrement(LocalKey(key), value);
         }
 
         public T Get<T>(string key)
         {
             var db = GetDatabase();
-            var redisValue = db.StringGet(key);
-            if (redisValue.HasValue)
-            {
-                return JsonSerializer.Deserialize<T>(redisValue);
-            }
-            return default(T);
+            var redisValue = db.StringGet(LocalKey(key));
+            return redisValue.HasValue ? JsonSerializer.Deserialize<T>(redisValue) : default(T);
         }
 
         public T Get<T>(string key, Func<T> func, int cacheTime = 30)
         {
             var db = GetDatabase();
-            var redisValue = db.StringGet(key);
+            var redisValue = db.StringGet(LocalKey(key));
 
             if (redisValue.HasValue)
             {
@@ -58,19 +55,24 @@ namespace AmBlitz.RedisCache
         public long Increment(string key, long value)
         {
             var db = GetDatabase();
-            return db.StringIncrement(key, value);
+            return db.StringIncrement(LocalKey(key), value);
         }
 
         public bool Remove(string key)
         {
             var db = GetDatabase();
-            return db.KeyDelete(key);
+            return db.KeyDelete(LocalKey(key));
         }
 
         public bool Set<T>(string key, T data, int cacheTime = 30)
         {
             var db = GetDatabase();
-            return db.StringSet(key, JsonSerializer.Serialize(data), TimeSpan.FromMinutes(cacheTime));
+            return db.StringSet(LocalKey(key), JsonSerializer.Serialize(data), TimeSpan.FromMinutes(cacheTime));
+        }
+
+        private static string LocalKey(string key)
+        {
+            return Prefix + key;
         }
     }
 }
