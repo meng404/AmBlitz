f082ce48eac9f82d2f3f5a196e0ca46ed691bde4
anmeng.wei <anmeng.wei@31huiyi.com>
2018-03-05 14:00:02 +0800

    am

--- AmBlitz/Properties/AssemblyInfo.cs
+++ AmBlitz/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("AmBlitz")]
-[assembly: AssemblyDescription("AmBlitz 手脚架")]
+[assembly: AssemblyDescription("AmBlitz")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AmBlitz")]
